f588a9fca76d6e75dd11b92201b4cba8c32ecef2
AlbedoUniversal <Albedogforce@gmail.com>
2019-07-23 19:18:02 +0300

    slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-console.log('test');
+$('.stocks__slider').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  prevArrow: '<img class="slick-prev" src="../images/s6/arrow.png">',
+  nextArrow: '<img class="slick-next" src="../images/s6/arrow.png">',
+  dots: true
+});
