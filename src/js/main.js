007ee434502e97202e38a87884fc7920f5a52dc5
AlbedoUniversal <Albedogforce@gmail.com>
2019-07-24 17:41:02 +0300

    slidertwook

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
-$('.stocks__slider').slick({
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  prevArrow: '<img class="slick-prev" src="../images/s6/arrow.png">',
-  nextArrow: '<img class="slick-next" src="../images/s6/arrow.png">',
-  dots: true
+$(document).ready(function () {
+  $('.stocks__slider').slick({
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      prevArrow: '<img class="slick-prev" src="../images/s6/arrow.png">',
+      nextArrow: '<img class="slick-next" src="../images/s6/arrow.png">',
+      dots: true
+    });
+  
+    $('.reviews-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      prevArrow: '<img class="slick-prev" src="../images/s9/arrow.png">',
+      nextArrow: '<img class="slick-next" src="../images/s9/arrow.png">',
+      dots: true
+    });
 });
