04a3d5f2d776f987cbd8d763cebec0fd95ca70bf
AlbedoUniversal <Albedogforce@gmail.com>
2019-08-09 15:16:47 +0300

    100%

--- src/js/main.js
+++ src/js/main.js
@@ -125,12 +125,10 @@ $(document).ready(function () {
     'packs-content__configuration-pictures__item'
   );
 
-  let activeTab;
-
   for (let i = 0; i < packsConfItems.length; i++) {
     packsConfItems[i].addEventListener('click', function () {
       const detaiLink = this.dataset.detail;
-      activeTab = this.parentNode.parentNode.parentNode;
+      const activeTab = this.parentNode.parentNode.parentNode;
       const detail = activeTab.querySelector(
         `[data-detail-show="${detaiLink}"]`
       );
@@ -139,11 +137,6 @@ $(document).ready(function () {
       );
 
       activeTab.getElementsByClassName('packs-content__configuration-pictures__orangeball')[0].classList.toggle('show');
-      activeTab.getElementsByClassName('packs-content__configuration-pictures__orangeball')[0].classList.toggle('show',false);
-
-      // packsDetailsInner[i].addEventListener('click', function() {
-      // })
-      
 
       for (let j = 0; j < packsDetailsInner.length; j++) {
         packsDetailsInner[j].classList.remove("active");
@@ -153,6 +146,14 @@ $(document).ready(function () {
 
     });
   }
+
+  const orangeBalls = document.getElementsByClassName('packs-content__configuration-pictures__orangeball');
+
+  Array.from(orangeBalls).forEach((ball) => {
+    ball.addEventListener('click', function () {
+      this.classList.toggle('show');
+    });
+  });
 
   // Scroll Gallery
   $('.mcs-horizontal-example').mCustomScrollbar({
